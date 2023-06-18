27e29867744f7f549096b9cd9be353496817987d
Rafael Bezerra <staimeer@gmail.com>
2023-06-18 16:27:53 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "The name for the bucket"
+  type        = string
+  default     = "test"
+}
+
+#variable "project" {
+#  description = "The name for the project"
+#  type        = string
+#  default     = "test"
+#}
