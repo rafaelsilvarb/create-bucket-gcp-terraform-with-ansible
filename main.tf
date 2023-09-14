f7ca97ef35f573dc9b99fa7bac13841d197d37b0
Rafael Silva <staimeer@gmail.com>
2023-09-14 11:40:20 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 resource "google_storage_bucket" "static" {
  name          = var.name
  location      = var.location
- storage_class = "STANDARD"
+ storage_class = "REGIONAL"
  project       = var.project
 
  uniform_bucket_level_access = true
